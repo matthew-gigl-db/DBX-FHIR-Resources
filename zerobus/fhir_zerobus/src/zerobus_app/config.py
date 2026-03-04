23d4e1b0c6b715e2d626f4006a2450176d3cb284
matthew-gigl-db <matthew.giglia@databricks.com>
2026-03-04 13:34:10 +0000

    update config

--- zerobus/fhir_zerobus/src/zerobus_app/config.py
+++ zerobus/fhir_zerobus/src/zerobus_app/config.py
@@ -2,18 +2,9 @@
 import os
 
 # Zerobus / Databricks settings (use Databricks secrets in production)
-ZEROBUS_SERVER_ENDPOINT = os.getenv(
-    "ZEROBUS_SERVER_ENDPOINT",
-    "7474651703425732.zerobus.us-east-1.cloud.databricks.com",
-)
-WORKSPACE_URL = os.getenv(
-    "WORKSPACE_URL",
-    "https://fe-sandbox-himss2026.cloud.databricks.com/",
-)
-FHIR_BUNDLE_TABLE_NAME = os.getenv(
-    "FHIR_BUNDLE_TABLE_NAME",
-    "himss.redox.fhir_bundle_zerobus",
-)
-
+ZEROBUS_SERVER_ENDPOINT = os.getenv("ZEROBUS_SERVER_ENDPOINT")
 CLIENT_ID = os.getenv("ZEROBUS_CLIENT_ID")
 CLIENT_SECRET = os.getenv("ZEROBUS_CLIENT_SECRET")
+WORKSPACE_URL = os.getenv("WORKSPACE_URL")
+
+FHIR_BUNDLE_TABLE_NAME = os.getenv("FHIR_BUNDLE_TABLE_NAME")
